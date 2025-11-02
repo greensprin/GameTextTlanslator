9c0c7de5a8366d4cc6e96b8c6b111180a16c200e
masashi momiki <mngjxd03170317@gmail.com>
2025-11-03 01:19:39 +0900

    shortモードの実装

--- text_translate.py
+++ text_translate.py
@@ -61,12 +61,16 @@ class GUI:
         self.combbox_select_mode_values = ("Full", "Short")
         self.combbox_select_mode = ttk.Combobox(width = 12, height=7, values=self.combbox_select_mode_values)
         self.combbox_select_mode.set(self.combbox_select_mode_values[0]) # Fullを初期値として設定
+        self.combbox_select_mode.bind("<<ComboboxSelected>>", self.__on_select_mode)
 
         # 言語選択プルダウンメニュー
         label_language = tk.Label(text="D. Select Language")
         self.combbox_language_values_1 = ("1.Key", "6.english","8.german","9.latam","10.french","11.italian","12.japanese","13.koreana","14.polish","15.brazilian","16.russian","17.turkish","18.schinese","19.tchinese","20.spanish")
         self.combbox_language = ttk.Combobox(width = 12, height=7, values=self.combbox_language_values_1)
         self.combbox_language.set(self.combbox_language_values_1[0]) # Keyを初期値として設定
+        self.combbox_language2 = ttk.Combobox(width = 12, height=7, values=self.combbox_language_values_1)
+        self.combbox_language2.set(self.combbox_language_values_1[0]) # Keyを初期値として設定
+        self.label_arrow = tk.Label(text="->", width="2")
 
         # 出力先ファイル表示
         self.label_output      = tk.Label(text="E. Output File Path")
@@ -87,18 +91,24 @@ class GUI:
         self.label_progress      = tk.Label(text="")
         self.label_progress_line = tk.Label(text="")
 
-        self.label_template         .grid(row=0 , column=0, padx=10, pady=(10, 0), sticky=tk.W)
-        self.text_header_template_0 .grid(row=4 , column=0, padx=10, pady=0, columnspan=5, sticky=tk.W)
-        label_file_path             .grid(row=5 , column=0, padx=10, pady=(15, 10), sticky=tk.W)
-        self.textbox_file_path      .grid(row=5 , column=1, padx=10, pady=(15, 10), sticky=tk.W)
-        self.filedialog_btn         .grid(row=5 , column=2, padx= 0, pady=(5, 0)    , sticky=tk.W)
-        label_select_mode           .grid(row=6 , column=0, padx=10, pady=10    , sticky=tk.W)
-        self.combbox_select_mode    .grid(row=6 , column=1, padx=10, pady=10    , sticky=tk.W)
-        label_language              .grid(row=7 , column=0, padx=10, pady=10    , sticky=tk.W)
-        self.combbox_language       .grid(row=7 , column=1, padx=10, pady=10    , sticky=tk.W)
-        self.label_output           .grid(row=8 , column=0, padx=10, pady= 5    , sticky=tk.W)
-        self.button                 .grid(row=9, column=0, padx=10, pady=(10,0),sticky=tk.W)
-        self.label_seconds_per_line .grid(row=10, column=0, padx=10, pady=0    , sticky=tk.W)
+        self.label_template         .grid(row=0 , column=0, padx= 10,  pady=(10, 0)               , sticky=tk.W)
+        self.text_header_template_0 .grid(row=4 , column=0, padx= 10,  pady=0       , columnspan=5, sticky=tk.W)
+        label_file_path             .grid(row=5 , column=0, padx= 10,  pady=(15, 10)              , sticky=tk.W)
+        self.textbox_file_path      .grid(row=5 , column=1, padx= 10,  pady=(15, 10), columnspan=5, sticky=tk.W)
+        self.filedialog_btn         .grid(row=5 , column=2, padx= 150, pady=(5, 0)                , sticky=tk.W)
+        label_select_mode           .grid(row=6 , column=0, padx= 10,  pady=10                    , sticky=tk.W)
+        self.combbox_select_mode    .grid(row=6 , column=1, padx= 10,  pady=10                    , sticky=tk.W)
+        label_language              .grid(row=7 , column=0, padx= 10,  pady=10                    , sticky=tk.W)
+        self.combbox_language       .grid(row=7 , column=1, padx= 10,  pady=10                    , sticky=tk.W)
+        self.label_arrow            .grid(row=7 , column=2, padx=  0,  pady=10                    , sticky=tk.W)
+        self.combbox_language2      .grid(row=7 , column=2, padx= 30,  pady=10                    , sticky=tk.W)
+        self.label_output           .grid(row=8 , column=0, padx= 10,  pady= 5                    , sticky=tk.W)
+        self.button                 .grid(row=9 , column=0, padx= 10,  pady=(10,0)                , sticky=tk.W)
+        self.label_seconds_per_line .grid(row=10, column=0, padx= 10,  pady=0                     , sticky=tk.W)
+
+        # short用の選択肢を非表示にする
+        self.label_arrow.grid_remove()
+        self.combbox_language2.grid_remove()
 
         # 画面リサイズされた時の挙動設定
         self.root.bind("<Configure>", self.__resize_frame)
@@ -176,8 +186,18 @@ class GUI:
                 self.button["state"] = "disable"
 
                 print("[INFO] Process Start !!!")
-                text_file_path  = re.sub("\"$", "", re.sub("^\"", "", self.textbox_file_path.get())) # windows explorerの「パスのコピー」で絶対パスを使った場合に対応するため、先頭と末尾のダブルクォーテーションを削除している
-                select_language = re.sub("[0-9]+\.", "", self.combbox_language.get())
+                text_file_path   = re.sub("\"$", "", re.sub("^\"", "", self.textbox_file_path.get())) # windows explorerの「パスのコピー」で絶対パスを使った場合に対応するため、先頭と末尾のダブルクォーテーションを削除している
+                select_language  = re.sub("[0-9]+\.", "", self.combbox_language .get())
+                select_language2 = re.sub("[0-9]+\.", "", self.combbox_language2.get())
+                select_mode      = self.combbox_select_mode.get()
+
+                # Shortモードで、翻訳先言語が元の言語と同じ場合はエラーを返す
+                if (select_mode == "Short" and select_language == select_language2):
+                    print("[ERROR] In Short Mode, the target language cannot be the same as the source language.")
+                    self.start_flg = 0
+                    self.filedialog_btn["state"] = "normal"
+                    self.button["state"] = "normal"
+                    continue
 
                 # 各設定値が空白の時はエラーを返す
                 if (text_file_path == ""):
@@ -207,7 +227,7 @@ class GUI:
                 self.output_filename = text_file_path # f"{os.path.splitext(text_file_path)[0]}_trans.txt"
                 self.label_outfilename["text"] = f"G. {self.output_filename}" # 出力ファイル名を表示
 
-                self.__run_text_translate(text_file_path, select_language)
+                self.__run_text_translate(text_file_path, select_language, select_language2, select_mode)
 
                 self.start_flg = 0
                 print("[INFO] Process End...")
@@ -222,7 +242,16 @@ class GUI:
             if (self.start_flg == 0 and GUI_END_FLG == 1):
                 break
 
-    def __run_text_translate(self, text_file_path, select_language):
+    def __on_select_mode(self, event):
+        select_mode = self.combbox_select_mode.get()
+        if (select_mode == "Full"):
+            self.label_arrow.grid_remove()
+            self.combbox_language2.grid_remove()
+        elif (select_mode == "Short"):
+            self.label_arrow.grid()
+            self.combbox_language2.grid()
+
+    def __run_text_translate(self, text_file_path, select_language, select_language2, select_mode):
         global GUI_END_FLG
 
         self.label_converting   ["text"] = "[ H. Translating ]"
@@ -232,7 +261,7 @@ class GUI:
         process_start_time = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
 
         # テキストデータ読み込み
-        text_header, text_dict_lists = self.__read_text(text_file_path)
+        text_header, text_dict_lists = self.__read_text(text_file_path, select_language, select_mode)
 
         # 翻訳
         translator = Translator()
@@ -240,6 +269,7 @@ class GUI:
             self.label_progress_line["text"] = f"Progressing line {i+1} of {len(text_dict_lists)}"
 
             # 空白行 or コメントアウト行は処理しない
+            print(item)
             if (item["Key"] == "" or item["Key"].find("<!--") != -1 or item["Key"] == "Key"):
                 print(f"[INFO] This row is CommentOut or empty row or multi key line. So, process is skiped.")
                 print(f"[INFO] {item}")
@@ -290,6 +320,10 @@ class GUI:
                 if (key_language == "Key"):
                     continue
 
+                if (select_mode == "Short" and key_language != select_language2):
+                    print(f"[INFO] Short Mode is selected. So, skip translating {key_language}.")
+                    continue
+
                 # 翻訳
                 item[key_language] = self.__run_googletrans(translator,
                                                             select_language_text,
@@ -329,7 +363,11 @@ class GUI:
                     f.write("<!-- The line below is skipping translation because the selected language is blank -->\n")
 
                 # 翻訳した内容を書き込む
-                f.writelines(",".join([str(value) for value in item.values()]) + "\n")
+                if (select_mode == "Full"):
+                    f.writelines(",".join([str(value) for value in item.values()]) + "\n")
+                else:
+                    write_value = f"{item['Key']},{item[select_language2]}\n" if (select_language2 in item) else ",".join([str(value) for value in item.values()]) + "\n"
+                    f.writelines(write_value)
                 
             # GUIが終了してたら、GUI Labelに書き込む記述を実行する前にreturnしてあげる(固まらないように)
             if (GUI_END_FLG == 1):
@@ -368,7 +406,7 @@ class GUI:
 
         return trans_text
 
-    def __read_text(self, text_file_path):
+    def __read_text(self, text_file_path, select_language, select_mode):
         text_header = []
         text_elems  = []
 
@@ -398,12 +436,22 @@ class GUI:
 
         # データ扱いやすいように辞書型に変換し、変換したものをリストに登録する
         text_dict_lists = []
+
         for elem in text_elems:
             if (elem[0] == "" or elem[0].find("<!--") != -1):
                 text_dict_tmp = dict(zip(text_header, elem))
             else:
-                text_dict_tmp = dict(zip_longest(text_header, elem))
-                
+                if (select_mode == "Full"):
+                    text_dict_tmp = dict(zip_longest(text_header, elem))
+                else:
+                    # Keyの場合はenglishとして扱う
+                    select_language = select_language if (select_language != "Key") else "english"
+
+                    text_dict_tmp = {
+                        "Key": elem[0],
+                        select_language: elem[1],
+                    }
+
             text_dict_lists.append(text_dict_tmp)
 
         return text_header, text_dict_lists
